7cf96b9ffbfb8b3a8d77e4f64e79decced74bacc
uucyan <uuunouucyan@gmail.com>
2019-09-06 12:59:08 +0900

    html を pug に変換

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,18 +9,12 @@ module.exports = {
   },
   extends: [
     'standard',
-    // "eslint:recommended",
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     "plugin:vue/recommended",
-    // "plugin:prettier/recommended"
   ],
-  // required to lint *.vue files
   plugins: [
     'vue',
     'pug'
   ],
-  // add your custom rules here
   rules: {
     "indent": ["error", 2],
     "quotes": ["error", "single"],
@@ -31,9 +25,7 @@ module.exports = {
     "no-extra-semi": "error",
     "no-unexpected-multiline": "error",
     "no-unreachable": "error",
-    // "semi": [2, "never"],
     "no-console": "off",
     "vue/max-attributes-per-line": "off",
-    // "prettier/prettier": ["error", { "semi": false }]
   }
 }
