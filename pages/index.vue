b1cc8ca80248c203d723c219a0629f3074cba9d8
uucyan <uuunouucyan@gmail.com>
2019-09-08 12:52:00 +0900

    index.vue に移行と余計な要素削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,113 @@
 <template lang="pug">
-v-layout(column='', justify-center='', align-center='')
-  v-flex(xs12='', sm8='', md6='')
-    .text-center
-      logo
-      vuetify-logo
-    v-card
-      v-card-title.headline
-        | Welcome to the Vuetify + Nuxt.js template
-      v-card-text
-        p
-          | Vuetify is a progressive Material Design component framework for
-          | Vue.js. It was designed to empower developers to create amazing
-          | applications.
-        p
-          | For more information on Vuetify, check out the
-          a(href='https://vuetifyjs.com', target='_blank')  documentation
-          | .
-        p
-          | If you have questions, please join the official
-          a(href='https://chat.vuetifyjs.com/', target='_blank', title='chat')
-            | discord
-          | .
-        p
-          | Find a bug? Report it on the github
-          a(href='https://github.com/vuetifyjs/vuetify/issues', target='_blank', title='contribute')
-            | issue board
-          | .
-        p
-          | Thank you for developing with Vuetify and I look forward to bringing
-          | more exciting features in the future.
-        .text-xs-right
-          em
-            small — John Leider
-        hr.my-3
-        a(href='https://nuxtjs.org/', target='_blank')
-          | Nuxt Documentation
-        br
-        a(href='https://github.com/nuxt/nuxt.js', target='_blank')
-          | Nuxt GitHub
-      v-card-actions
-        v-spacer
-          v-btn(color='primary', nuxt='', to='/inspire')
-            | Continue
+v-layout.px-2.py-2.row.wrap
+  v-flex.lg6.sm12.xs12.text-center
+    canvas#canvas(:width='size.width' :height='size.height' ref='canvas')
+  v-flex.lg6.sm12.xs12.align-self-center(v-if='context != null')
+    v-text-field.textarea(v-model='name', outline hide-details label='名前', rows='3', color='#333', style='margin: auto;', @input='drawImage')
+    v-textarea.textarea(v-model='body', outline hide-details label='本文', rows='3', color='#333', style='margin: auto;', @input='drawImage')
+    v-layout
+      v-flex.mr-1
+        v-slider(v-model='font.size', label='フォントサイズ', color='#333', min='10', max='150', @input='drawImage')
+      v-flex.ml-2
+        v-text-field(v-model='font.size', hide-details color='#333', type='number', suffix='px', style='width: 56px;', @input='drawImage')
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data () {
+    return {
+      // フォントの設定
+      font: {
+        family: 'Meiryo',
+        size: 25,
+        lineHeightOffset: 4,
+      },
+      // TODO: 一旦固定にしてる
+      imgsrc: 'image/chisato_shirasagi.png',
+      // 実際に表示する画像サイズ（Canvas サイズもこれを使用する）
+      size: {
+        width: 525,
+        height: 788,
+      },
+      context: null,
+      name: '',
+      body: '',
+    }
+  },
+  mounted () {
+    // Canvas の初期化
+    this.initCanvas()
+    // 画像描画
+    this.drawImage()
+  },
+  methods: {
+    /**
+     * Canvas の初期化
+     */
+    initCanvas () {
+      this.context = this.$refs.canvas.getContext('2d')
+      this.context.textAlign = 'left'
+      this.context.textBaseline = 'middle'
+    },
+
+    /**
+     * 画像に名前を描画
+     */
+    drawName () {
+      this.context.fillStyle = '#ffffff'
+      this.context.fillText(this.name, 65, 616.1)
+    },
+
+    /**
+     * 画像に本文を描画
+     */
+    drawBody () {
+      this.context.fillStyle = '#000000'
+      const body = this.body
+
+      let lines = [body]
+
+      if (body.includes('\n')) {
+        lines = body.split('\n')
+      }
+
+      lines.forEach((line, idx) => {
+        this.context.fillText(
+          line,
+          70, // X 座標
+          657 + idx * (this.font.size + this.font.lineHeightOffset) // Y 座標
+        )
+      })
+    },
+
+    /**
+     * 画像の描画
+     */
+    drawImage () {
+      // Canvas のクリア
+      this.context.clearRect(0, 0, this.size.width, this.size.height)
+
+      const img = new Image()
+      img.src = this.imgsrc
+      img.onload = () => {
+        this.context.drawImage(img, 0, 0, this.size.width, this.size.height)
+        this.context.font = `${this.font.size}px '${this.font.family}'`
+        this.drawName()
+        this.drawBody()
+      }
+    },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+#canvas {
+  @media screen and (max-width: 525px) {
+    width: 100%;
+  }
+}
+
+.textarea {
+  font-size: 18px;
+}
+</style>
