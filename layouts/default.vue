7ae27b4dc99225a617f3cdba5058dfe8b9fba23a
uucyan <uuunouucyan@gmail.com>
2019-10-14 22:38:14 +0900

    下にあるナビゲーションをコンポーネント化

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,21 @@
 <template lang="pug">
 v-app(dark)
   v-content.body
-    v-app-bar(collapse absolute scroll-target="#playground-example")
+    v-app-bar(collapse absolute scroll-target="#container")
       img.logo(src="~/assets/image/logo.svg")
 
-    v-container#playground-example.overflow-y-auto(style='max-height: 100vh; max-width: 100vw; padding-bottom: 80px;')
+    v-container#container.overflow-y-auto(style='max-height: 100vh; max-width: 100vw; padding-bottom: 80px;')
       nuxt
 
-    v-bottom-navigation(scroll-target='#playground-example' absolute grow color='teal')
-      v-btn
-        span サイトについて
-        v-icon mdi-information-outline
-      v-btn
-        span 保存
-        v-icon mdi-download
-      v-bottom-sheet(v-model='sheet', hide-overlay)
-        template(v-slot:activator='{ on }')
-          v-btn(v-on='on')
-            span キャラ変更
-            v-icon mdi-image-filter
-        v-sheet.text-center
-          v-btn.mt-6(text='', color='error', @click='sheet = !sheet') 閉じる
+    Navigation
 </template>
 
 <script>
+import Navigation from '~/components/Navigation'
 
 export default {
-  data () {
-    return {
-      title: 'バンドリ！ セリフジェネレーター！',
-      sheet: false,
-    }
+  components: {
+    Navigation,
   },
 }
 </script>
