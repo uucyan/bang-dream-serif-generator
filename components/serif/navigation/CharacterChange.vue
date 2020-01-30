81fe8313a00d182f8a0bc4cd39ea3710dfb52104
h-marei <h.marei0803@gmail.com>
2020-01-30 20:08:09 +0900

    一旦コミット

--- components/serif/navigation/CharacterChange.vue
+++ components/serif/navigation/CharacterChange.vue
@@ -4,7 +4,20 @@ v-bottom-sheet(v-model='sheet', hide-overlay)
     v-btn(v-on='on')
       span キャラ変更
       v-icon mdi-image-filter
+
   v-sheet.text-center
+    v-row
+      v-col(cols='12', sm='6', offset-sm='3')
+        v-card
+          v-container(fluid='')
+            v-row
+              v-col.d-flex.child-flex(v-for='n in 18', :key='n', cols='4')
+                v-card.d-flex(flat='', tile='')
+                  v-img.grey.lighten-2(:src='`https://picsum.photos/500/300?image=${n * 5 + 10}`', :lazy-src='`https://picsum.photos/10/6?image=${n * 5 + 10}`', aspect-ratio='1')
+                    template(v-slot:placeholder='')
+                      v-row.fill-height.ma-0(align='center', justify='center')
+                        v-progress-circular(indeterminate='', color='grey lighten-5')
+
     v-btn.mt-6(text='', color='error', @click='sheet = !sheet') 閉じる
 </template>
 
@@ -20,4 +33,7 @@ export default {
 </script>
 
 <style lang="scss" scope>
+.sheet {
+  overflow-x: auto;
+}
 </style>
